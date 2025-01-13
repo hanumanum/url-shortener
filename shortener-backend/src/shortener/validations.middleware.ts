7e5097febc5a0cb8bf77d2bc15f103507c834497
hanuman <a.kolyan@gratun.am>
2025-01-13 20:21:00 +0400

    some improvements, but not completed

--- shortener-backend/src/shortener/validations.middleware.ts
+++ shortener-backend/src/shortener/validations.middleware.ts
@@ -18,7 +18,6 @@ const validateUrl: RequestHandler = (req: Request, res: Response, next: NextFunc
     next();
 }
 
-
 const validateSlug: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
     const slug = req.params?.slug;
     const errors = slugValidations(slug);
@@ -35,22 +34,34 @@ const validateSlug: RequestHandler = (req: Request, res: Response, next: NextFun
     next();
 }
 
-//TODO: implement this function
+//TODO: fix this middleware
+// If works as expected, it should check if the URL is available
+// but in some cases (facebook) it returns 400 even if the URL is available
 const isURLAvailable: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     next();
-
     return;
     try {
         const { url } = req.body;
-        const response = await fetch(url);
+        
+        const customHeaders = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'Referer': 'https://www.google.com/'
+        };
+        
+        const response = await fetch(url, { 
+            method: 'HEAD',
+            headers: customHeaders,
+        });
+
         if (!response.ok) {
+            console.log(response);
             res.status(400).json({ message: 'URL is currently unavaliable' });
             return;
         }
         next();
     } catch (error) {
         logger.logError('error', error as Error, { url: req.body.url });
-        res.status(500).json({ message: 'URL is currently unavaliable', errors: [error] });
+        res.status(400).json({ message: 'URL is currently unavaliable', errors: [error] });
         return;
     }
 }
